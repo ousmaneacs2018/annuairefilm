8e3bc8bbebf4cedb7a5022c164335fcaa82d9b6c
alexandreb21000 <38067258+alexandreb21000@users.noreply.github.com>
2018-05-28 13:42:19 +0200

    Part CSS
    Première partie des fichiers CSS avec Materialize

---
+++ Film.php
@@ -0,0 +1,27 @@
+<?php
+
+class Film extends Model {
+   public $id_film, $fil_titre, $fil_annee, $fil_affiche;
+
+   public static function getFromSlug( $slug ) {
+      $db = Database::getInstance();
+      $sql = "SELECT * FROM film WHERE slug = :slug";
+      $stmt = $db->prepare($sql);
+      $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      $stmt->bindValue(':slug', $slug, PDO::PARAM_STR);
+      $stmt->execute();
+      return $stmt->fetch();
+   }
+
+   public static function getList() {
+      $db = Database::getInstance();
+      $sql = "SELECT * FROM film
+              order by rand()  
+              limit 5";
+      $stmt = $db->query($sql);
+      $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      return $stmt->fetchAll();
+
+   }
+
+}
